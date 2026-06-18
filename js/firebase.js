fc0aa89cb60ab8758b61a094bfe5c1616ab2ff3f
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-06-18 15:50:02 +0300

    Upgrade Firebase SDK to version 12.15.0

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4-9M6xgTHfwDdG6APm1Ha_g7TyQJfD_c",
   authDomain: "geresu-dhuki-sacco-d4de7.firebaseapp.com",
   projectId: "geresu-dhuki-sacco-d4de7",
-  storageBucket: "geresu-dhuki-sacco-d4de7.appspot.com",
+  storageBucket: "geresu-dhuki-sacco-d4de7.firebasestorage.app",
   messagingSenderId: "379938868750",
-  appId: "1:379938868750:web:782f5ff7f65665ae1cf65f"
+  appId: "1:379938868750:web:782f5ff7f65665ae1cf65f",
+  measurementId: "G-J309NM1J9G"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
 
-// KEEP SESSION
-setPersistence(auth, browserLocalPersistence);
+const db = getFirestore(app);
+
+export { app, auth, db };
